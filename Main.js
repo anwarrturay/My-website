624606703eb9714904491b60d7e21d595f807c2e
Anwarr O.B Turay <anwarrturay@icloud.com>
2023-06-29 14:40:17 +0100

    This is my website project

---
+++ Main.js
@@ -0,0 +1,4 @@
+var firstName = document.getElementById("fname");
+var lastName = document.getElementById("lname");
+var email = document.getElementById("Email");
+var phoneNumber = document.getElementById("phoneNumber");
